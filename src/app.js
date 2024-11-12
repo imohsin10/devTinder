1f09e8b8e312e3b7112a41b70d3033b0c4a78b77
imohsin10 <mohsinabbassayyed@gmail.com>
2024-11-12 17:33:13 +0530

    exploring get post

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 const express=require ('express');
 const app=express();
 
-app.use("/hello",(req,res)=>{
-    res.send("hello from the server and me")
-})
+// will match all the method to /user
 app.use("/test",(req,res)=>{
-    res.send("hello from the server")
+    res.send("test1")
+})
+// will match only get  method to /test
+app.get("/user",(req,res)=>{
+    res.send({firstname:"moh",age:17})
+
+})
+app.post("/user",(req,res)=>{
+    res.send("data saved succesfully")
 })
 app.listen(3000,()=>{
     console.log("server is succesfully running on port 3000")
