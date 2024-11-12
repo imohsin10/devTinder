87904fc58209218d94c153b9ac93ee4e08267573
imohsin10 <mohsinabbassayyed@gmail.com>
2024-11-12 17:46:03 +0530

    playing with route

--- src/app.js
+++ src/app.js
@@ -1,17 +1,13 @@
 const express=require ('express');
 const app=express();
 
-// will match all the method to /user
-app.use("/test",(req,res)=>{
-    res.send("test1")
-})
+
 // will match only get  method to /test
-app.get("/user",(req,res)=>{
+app.get("/user/:userid",(req,res)=>{
+   console.log(req.query) ;
+   console.log(req.params) ;
     res.send({firstname:"moh",age:17})
 
-})
-app.post("/user",(req,res)=>{
-    res.send("data saved succesfully")
 })
 app.listen(3000,()=>{
     console.log("server is succesfully running on port 3000")
